4759a16a4435cded931ad4caa3f4e8e3bae09a9e
Alexandr Fonari <juice@juice.local>
2016-09-19 19:36:48 -0400

    Add test infrastructure

---
+++ test/test_vasp_raman.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+import os
+import time
+import unittest
+import vasp_raman
+
+class VaspRamanTester(unittest.TestCase):
+    def testMAT_m_VEC(self):
+        self.assertTrue(False)
